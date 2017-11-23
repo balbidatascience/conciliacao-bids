eec2b84e0b7d2bf70f8e1f31492059ab8a64df33
LAPTOP\gabriel <balbidatascience@gmail.com>
2017-11-23 11:26:56 -0200

    finalixado

--- Scripts/ScriptConciliacaoVendaAtivaLegado.sql
+++ Scripts/ScriptConciliacaoVendaAtivaLegado.sql
@@ -1,5 +1,6 @@
 
 
+
 --===============================================================================
 -- analisando valores por lote
 -- drop table #tmp
@@ -22,11 +23,11 @@ select  c.LoteUnico,
 from fatConciliacaoLoteVenda a
 inner join dimTempo b on a.IdDataVenda = b.IdTempo
 inner join dimLoteVenda c on a.IdLoteVenda = c.IdLoteVenda
-where b.DATA <= '10/20/2017'
+where b.DATA between '1/1/2017' and '10/20/2017'
 group by c.LoteUnico--, b.DATA
 --order by b.DATA
 
-select * from #tmp where ValorVenda > 0 
+select * from #tmp where ValorVenda = 0 
 --===============================================================================
  
 
@@ -36,41 +37,19 @@ select * from #tmp where ValorVenda > 0
 
  --truncate table fatConciliacaoLoteVenda
 
-select * from dsVendaAtivaBileto where numero_autorizacao_adquirente like '%671532%'
-select * from dsVendaAtivaLegado where numero_autorizacao_adquirente like '%671532%'
+select * from dsVendaAtivaBileto where numero_autorizacao_adquirente like '%360293%'
+select * from dsVendaAtivaLegado where numero_autorizacao_adquirente like '%360293%'
 
-select * from dsVendaAtivaBileto where numero_autorizacao_adquirente like '%651521%'
-select * from dsVendaAtivaLegado where numero_autorizacao_adquirente like '%651521%'
+select * from dsVendaAtivaBileto where numero_autorizacao_adquirente like '%360293%'
+select * from dsVendaAtivaLegado where numero_autorizacao_adquirente like '%360293%'
 
 select b.IdTransacaoAdquirente, a.* from dimTransacaoIR a
 left join fatTransacaoIR b on a.IdTransacaoIR = b.IdTransacaoAdquirente
 where Localizador like '%6520352082%'
 
-select * from dimLoteVenda where IdLoteVenda = 107299
-select * from dimLoteVenda where LoteUnico = '170035100204846'
+select * from dimLoteVenda where LoteUnico = '171297450554050931'
 
--- 208 -- Apenas 48 foram Identificadas
-select * from fatConciliacaoVenda where IdTransacaoAdquirente in (
-select distinct IdTransacaoAdquirente
-from dimTransacaoAdquirente 
-where LoteUnico = '170035100204846') 
-
-
-select sum(ValorVenda), sum(ValorCanceladoVenda), sum(ValorTransacionadoAdquirente), sum(ValorCanceladoAdquirente) from fatConciliacaoVenda where IdLoteVenda in (
-101193,
-107299,
-138890)
-
-select sum(ValorVenda) as ValorVenda, 
-		sum(ValorCanceladoVenda) as ValorCanceladoVenda, 
-		sum(ValorTransacionadoAdquirente) as ValorTransacionadoAdquirente, 
-		sum(ValorCanceladoLiquido) as ValorCanceladoLiquido, 
-		sum(ValorPrevistoConciliador) as ValorPrevistoConciliador, 
-		sum(ValorPago) as ValorPago 
-from fatConciliacaoLoteVenda where IdLoteVenda in (
-101193,
-107299,
-138890)
+select * from dsFluxoCaixaHist where LoteUnico = '171297450554050931'
 
 
 select a.* 
