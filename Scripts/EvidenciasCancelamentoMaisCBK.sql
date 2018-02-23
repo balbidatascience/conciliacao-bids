b34f250ad663e0f2f8b74e5d2d813369087d865c
LAPTOP-HTDQFPRG\gabri <gabrielopes@gmail.com>
2018-02-23 10:04:59 -0300

    last version

--- Scripts/EvidenciasCancelamentoMaisCBK.sql
+++ Scripts/EvidenciasCancelamentoMaisCBK.sql
@@ -1,8 +1,9 @@
 use IR
 
-select d.TipoCancelamento, d.NSU, 
+select  d.Adquirente, 
+		d.TipoCancelamento, d.NSU, 
 		d.NroAutorizacao, d.TID, 
-		d.Adquirente, d.Bandeira, 
+		d.Bandeira, 
 		d.Estabelecimento, d.NroCartao, 
 		d.Tipo, convert(varchar, d.DtVenda, 103) as DtVenda, 
 		convert(varchar, d.DtCancelamento, 103) as DtCancelamento, d.VlrBruto, 
@@ -12,6 +13,10 @@ inner join fatTransacaoAdquirente b on a.IdTransacaoAdquirente = b.IdTransacaoAd
 inner join fatCancelamento c on b.IdTransacaoAdquirente = c.IdTransacaoAdquirente
 inner join dimCancelamento d on c.IdCancelamento = d.IdCancelamento
 where b.IdStatusCancelamento = 5
+and d.DtVenda >= '6/10/2017'
 order by Adquirente, a.IdTransacaoAdquirente, d.DtCancelamento
 
+
+select * from dimTransacaoAdquirente where TID = '8eb1031e676f489b'
+select * from fatTransacaoAdquirente where IdTransacaoAdquirente = 
 
